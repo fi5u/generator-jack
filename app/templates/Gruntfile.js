028671664e3812bc68928faa90d38463335490b3
Tommy Fisher <tommybfisher@gmail.com>
2013-10-14 14:31:27 +0300

    Dist version moves JavaScript libraries from bower_components to js/lib

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -38,8 +38,20 @@ module.exports = function (grunt) {
 
             dist: {
                 files: [
-                    {expand: true, cwd: '<%%= yeoman.app %>', src: ['**', '!**/scss/**'], dest: '<%%= yeoman.dist %>'},
-                    {expand: true, cwd: '<%%= yeoman.app %>/assets/scss/fonts', src: ['**'], dest: '<%%= yeoman.dist %>/assets/css/fonts'}
+                    {expand: true, cwd: '<%%= yeoman.app %>', src: ['**', '!**/scss/**', '!**/js/*.js', '!**/bower_components/**'], dest: '<%%= yeoman.dist %>'},
+                    {expand: true, cwd: '<%%= yeoman.app %>/assets/scss/fonts', src: ['**'], dest: '<%%= yeoman.dist %>/assets/css/fonts'},
+                    {expand: true, cwd: '<%%= yeoman.app %>/assets/bower_components/jquery', src: ['jquery.min.js'], dest: '<%%= yeoman.dist %>/assets/js/lib', rename: function (dest) {
+                        var jQconf = grunt.file.readJSON('app/assets/bower_components/jquery/bower.json');
+                        return dest + '/jquery.' + jQconf.version + '.min.js';
+                    }},
+                    {expand: true, cwd: '<%%= yeoman.app %>/assets/bower_components/jquery-migrate', src: ['jquery-migrate.min.js'], dest: '<%%= yeoman.dist %>/assets/js/lib', rename: function (dest) {
+                        var jqMigconf = grunt.file.readJSON('app/assets/bower_components/jquery-migrate/.bower.json');
+                        return dest + '/jquery-migrate.' + jqMigconf.version + '.min.js';
+                    }},
+                    {expand: true, cwd: '<%%= yeoman.app %>/assets/bower_components/jquery-legacy', src: ['jquery.min.js'], dest: '<%%= yeoman.dist %>/assets/js/lib', rename: function (dest) {
+                        var jQconf = grunt.file.readJSON('app/assets/bower_components/jquery-legacy/.bower.json');
+                        return dest + '/jquery.' + jQconf.version + '.min.js';
+                    }}
                 ]
             }
         },
