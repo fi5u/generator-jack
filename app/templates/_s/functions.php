4d5275da29079c0ef1d33687d2ae509a0790c804
tommy.fisher@creuna.fi <tommybfisher@gmail.com>
2014-01-03 15:09:28 +0200

    WP: Used proper register and enqueuing of scripts

--- app/templates/_s/functions.php
+++ app/templates/_s/functions.php
@@ -65,21 +65,32 @@ add_action( 'after_setup_theme', '_s_setup' );
  * Enqueue scripts and styles
  */
 function _s_scripts() {
-    wp_enqueue_style( '_s-style', get_template_directory_uri() . '/style.css' );
+    wp_register_style( '_s-style', get_template_directory_uri() . '/style.css' );
+    wp_register_style( '_s-lteie8-style', get_template_directory_uri() . '/assets/css/lteie8.css' );
 
-    wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+    wp_register_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20140101', true );
+    wp_register_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20140101', true );
+    wp_register_script( '_s-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20140101' );
+    wp_register_script( '_s-js-script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '20140101', true );
 
-    wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+    wp_enqueue_style( '_s-style' );
+    if( preg_match('/(?i)msie [2-8]/',$_SERVER['HTTP_USER_AGENT']) ) {
+        // if IE<=8
+        wp_enqueue_style( '_s-lteie8-style' );
+    }
+
+    wp_enqueue_script( '_s-navigation' );
+    wp_enqueue_script( '_s-skip-link-focus-fix' );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
     }
 
     if ( is_singular() && wp_attachment_is_image() ) {
-        wp_enqueue_script( '_s-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+        wp_enqueue_script( '_s-keyboard-image-navigation' );
     }
 
-    wp_enqueue_script( '_s-js-script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '20130115', true );
+    wp_enqueue_script( '_s-js-script' );
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
 
