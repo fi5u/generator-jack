0a3f140a4a3f17e8086522609eaa82b4f8c10336
tommy.fisher@creuna.fi <tommybfisher@gmail.com>
2014-04-03 08:46:32 +0300

    WP: added my-meta-box class

--- app/templates/_s/functions.php
+++ app/templates/_s/functions.php
@@ -220,6 +220,28 @@ if ( ! isset( $content_width ) ) $content_width = 1170;
  */
 
 
+/**
+ * Custom meta box class
+ * https://github.com/bainternet/My-Meta-Box/blob/master/class-usage-demo.php
+ */
+/*require_once("inc/meta-box-class/my-meta-box-class.php");
+
+if (is_admin()) {
+    $prefix = '_s_';
+    $config = array(
+        'id'             => 'demo_meta_box',          // meta box id, unique per meta box
+        'title'          => 'Simple Meta Box fields',          // meta box title
+        'pages'          => array('post', 'page'),      // post types, accept custom post types as well, default is array('post'); optional
+        'context'        => 'normal',            // where the meta box appear: normal (default), advanced, side; optional
+        'priority'       => 'high',            // order of meta box: high (default), low; optional
+        'fields'         => array(),            // list of meta fields (can be added by field arrays)
+        'local_images'   => false,          // Use local or hosted images (meta box images for add/remove)
+        'use_with_theme' => false          //change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
+        );
+    $my_meta =  new AT_Meta_Box($config);
+}*/
+
+
 /*=============================================================
  *                           MEDIA                            *
  *============================================================*/
